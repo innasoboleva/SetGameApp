ae30104118784d0f891ebd55f8c940bb76dba212
Inna Soboleva <nysik62@gmail.com>
2022-08-19 18:47:33 -0700

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Inna Soboleva on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
