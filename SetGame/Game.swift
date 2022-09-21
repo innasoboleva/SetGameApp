323cedc9ef43f2c30f389aaaac6182a65c22369e
Inna Soboleva <nysik62@gmail.com>
2022-09-20 19:04:13 -0700

    Cards now appear on the same spot where matched disappeared.

--- SetGame/Game.swift
+++ SetGame/Game.swift
@@ -58,19 +58,45 @@ struct Game {
                 for i in pickedCards {
                     cards[i].isMatched = true
                 }
-                for _ in 0...3 {
-                    if let toRemove = cards.indices.first(where: { cards[$0].isMatched }) {
-                        cards.remove(at: toRemove)
-                    }
-                }
-                if cards.count < Const.cardsVisible {
-                    addCards()
+                if cards.count <= Const.cardsVisible {
+                    addCard(at: pickedCards)
+                } else {
+                    removeCards()
                 }
             } else {
                 for i in pickedCards {
                     cards[i].isPicked = false
                 }
             }
+        }
+    }
+    
+    mutating func addCard(at arr: [Int]) {
+        let count = extraCards.count
+        if count > 0 && count >= 3 {
+            for i in 0...2 {
+                cards.remove(at: arr[i])
+                cards.insert(extraCards[i], at: arr[i])
+            }
+            for _ in 0...2 {
+                extraCards.remove(at: 0)
+            }
+        } else {
+            for i in 0..<count {
+                cards.remove(at: arr[i])
+                cards.insert(extraCards[i], at: arr[i])
+            }
+            for _ in 0..<count {
+                extraCards.remove(at: 0)
+            }
+            removeCards()
+        }
+    }
+    
+    mutating func removeCards() {
+        let toRemove = cards.indices.filter({ cards[$0].isMatched })
+        for i in toRemove {
+            cards.remove(at: i)
         }
     }
     
